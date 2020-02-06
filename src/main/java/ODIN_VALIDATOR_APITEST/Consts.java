25c92df3234420707717c458e70a74f975b23a55
Irimia <ciprian.irimia@metrosystems.net>
2020-02-06 14:49:29 +0200

    asdadas

--- src/main/java/ODIN_VALIDATOR_APITEST/Consts.java
+++ src/main/java/ODIN_VALIDATOR_APITEST/Consts.java
@@ -16,7 +16,6 @@ public class Consts {
 	public static final String VERSION_8 = "v_8_0";
 	public static final String VERSION_9 = "v_9_0";
 	//public static final String LINK_PROCESSOR = "https://request-processor.odin-dev.metrosystems.net/process/";
-
 	public static final String LINK_PROCESSOR = "https://request-processor.odin-"+System.getProperty("varlink")+".metrosystems.net/process/";
 	public static final String VERSION_PROCESSOR_1 = "v_2_0/";
 	public static final String VERSION_PROCESSOR_2 = "v_2.2_0/";
@@ -34,13 +33,8 @@ public class Consts {
 	
 	
 	public static final String COUNTRY_TENANT = "PL/MCC/SDA";
-
 	public static final String FILEPATH_VALIDATOR = "src/main/resources/validator/files/";
-	
 	public static final String PATH_EXPECTED_VALIDATOR = "src/main/resources/validator/expected/";
-	
 	public static final String FILEPATH_REQUEST_PROCESSOR = "src/main/resources/process_request/files/";
-
-	
 	public static final String PATH_EXPECTED_REQUEST_PROCESSOR = "src/main/resources/process_request/expected/";
 }
